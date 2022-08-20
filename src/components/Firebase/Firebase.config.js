955071cfaedd91be3c2825c24623729ff6f545d2
nasimit2020 <nasimitjesbd@gmail.com>
2022-08-20 12:22:11 +0600

    firebase config ignore

---
+++ src/components/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDBkl4c3wDkjTuBKWE7QVHbaIqNTAs0yPI",
+    authDomain: "duck-meet.firebaseapp.com",
+    projectId: "duck-meet",
+    storageBucket: "duck-meet.appspot.com",
+    messagingSenderId: "125622903346",
+    appId: "1:125622903346:web:cf13df497c70a1fd4b522b"
+};
+
+export default firebaseConfig;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,14 +7,8 @@ import { useContext } from 'react';
 import { UserContext } from '../../App';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Admin from '../Admin/Admin';
-const firebaseConfig = {
-    apiKey: "AIzaSyDBkl4c3wDkjTuBKWE7QVHbaIqNTAs0yPI",
-    authDomain: "duck-meet.firebaseapp.com",
-    projectId: "duck-meet",
-    storageBucket: "duck-meet.appspot.com",
-    messagingSenderId: "125622903346",
-    appId: "1:125622903346:web:cf13df497c70a1fd4b522b"
-};
+import firebaseConfig from '../Firebase/Firebase.config';
+
 
 const Login = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
